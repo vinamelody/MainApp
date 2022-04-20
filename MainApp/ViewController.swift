91438c61db2e690b28e5a39f320a3b237c3c84f2
Vina Melody <vina_melody@yahoo.com>
2022-04-20 16:37:20 +0800

    Initial Commit

---
+++ MainApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainApp
+//
+//  Created by Vina Rianti on 20/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
